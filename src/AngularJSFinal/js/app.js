76cf5384454523e07b18f25b94de0a47ecb3af96
Vivek Pai <VivekPai@live.com>
2014-11-17 01:03:58 -0500

    Cleaned up code

--- src/AngularJSFinal/js/app.js
+++ src/AngularJSFinal/js/app.js
@@ -65,101 +65,90 @@ var app = new Mhci.App('portfolio');
 /*
  * Portfolio Index Controller
  */
-//var PortfolioIndexController = (function () {
-	function PortfolioIndexController($scope, $sce, dataService) {
-		this.$inject = ['dataService'];
+function PortfolioIndexController($scope, $sce, dataService) {
+	this.$inject = ['dataService'];
 		
-		$scope['trustSrc'] = function (src) {
-			return $sce.trustAsResourceUrl(src);
-		};
+	$scope['trustSrc'] = function (src) {
+		return $sce.trustAsResourceUrl(src);
+	};
 		
-		//A function in data service that returns a list of portfolio pieces
-		dataService.getPortfolioPieces().then(function (request) {
-		    var works = request.data.Works;
-			$scope['Works'] = works;
-		});
+	//A function in data service that returns a list of portfolio pieces
+	dataService.getPortfolioPieces().then(function (request) {
+		var works = request.data.Works;
+		$scope['Works'] = works;
+	});
 		
-	}
+}
 	
-//	return PortfolioIndexController;
-//})();
 //Register controller
 app.registerController("/", "Home.html", PortfolioIndexController);
 
 /*
  * Portfolio Detail Controller
  */
-//var PortfolioDetailController = (function () {
-	function PortfolioDetailController($scope, $sce, $routeParams, dataService) {
-		this.$routeParams = $routeParams;
-		this.dataService = dataService;
-		this.$inject = ['ngSanitize', '$routeParams', 'dataService'];
+function PortfolioDetailController($scope, $sce, $routeParams, dataService) {
+	this.$routeParams = $routeParams;
+	this.dataService = dataService;
+	this.$inject = ['ngSanitize', '$routeParams', 'dataService'];
 		
-		var workId = $routeParams['id'];
+	var workId = $routeParams['id'];
         
-		dataService.getPortfolioDetails(workId).then(function (response) {
-			$scope['SelectedWorkData'] = response.data;
-		});
+	dataService.getPortfolioDetails(workId).then(function (response) {
+		$scope['SelectedWorkData'] = response.data;
+	});
 
 		
-		//Functions that enable you to embed youtube videos
-		$scope['trustSrc'] = function (src) {
-			return $sce.trustAsResourceUrl(src);
-		};
-		$scope['trustHtml'] = function (html) {
-			return $sce.trustAsHtml(html);
-		};
-		$scope.getYouTubeLink = function (videoId) {
-		    return 'https://www.youtube.com/embed/' + videoId;
-		};
-
-		//
-	}
-//	return PortfolioDetailController;
-//})();
+	//Functions that enable you to embed youtube videos
+	$scope['trustSrc'] = function (src) {
+		return $sce.trustAsResourceUrl(src);
+	};
+	$scope['trustHtml'] = function (html) {
+		return $sce.trustAsHtml(html);
+	};
+	$scope.getYouTubeLink = function (videoId) {
+		return 'https://www.youtube.com/embed/' + videoId;
+	};
+}
 //Register controller
 app.registerController("/Portfolio/:id", "Detail.html", PortfolioDetailController);
 
-//var AboutController = (function () {
-    function AboutController($scope) {
+/*
+ * About Controller
+ */
+function AboutController($scope) {
 
-    }
-//    return AboutController;
-//})();
-
+}
+//Register controller
 app.registerController("/About", "About.html", AboutController);
 
 /*
  * Menu Controller
  */
-//var MenuController = (function () {
-	function MenuController($scope, $location) {
-		this.$inject = ['$location'];
+function MenuController($scope, $location) {
+	this.$inject = ['$location'];
 		
-		//Applies a different CSS class based on the current URL
-		$scope['getMenuLinkClass'] = function (path) {
-			if ($location.path().substr(0, path.length) == path) {
-				return "active";
-			} else if ($location.path() == "/" && path == "/Work") {
-				return "active";
-			} else {
-				return "";
-			}
-		};
+	//Applies a different CSS class based on the current URL
+	$scope['getMenuLinkClass'] = function (path) {
+		if ($location.path().substr(0, path.length) == path) {
+			return "active";
+		} else if ($location.path() == "/" && path == "/Work") {
+			return "active";
+		} else {
+			return "";
+		}
+	};
 		
-	}
-//	return MenuController;
-//})();
+}
+
 //Registers controller
 app.registerControllerFactory("MenuController", MenuController);
 
+/*
+ * Main Controller
+ */
+function MainController($scope) {
 
-//var MainController = (function () {
-    function MainController($scope) {
-
-    }
-//    return MainController;
-//})();
+}
 
 app.registerControllerFactory("MainController", MainController);
 
@@ -167,25 +156,22 @@ app.registerControllerFactory("MainController", MainController);
 /*
  * DataService
  */
-//var JSONFileDataService = (function () {
-    function JSONFileDataService($q, $http) {
-        this.$q = $q;
-        this.$http = $http;
-        this.$inject = ['http'];
-    }
+function JSONFileDataService($q, $http) {
+    this.$q = $q;
+    this.$http = $http;
+    this.$inject = ['http'];
+}
 
-    //Gets list of portfolio pieces
-    JSONFileDataService.prototype.getPortfolioPieces = function () {
-        return this.$http.get('data/index.json');
-    };
+//Gets list of portfolio pieces
+JSONFileDataService.prototype.getPortfolioPieces = function () {
+    return this.$http.get('data/index.json');
+};
 
-    //Gets details for a single portfolio piece
-    JSONFileDataService.prototype.getPortfolioDetails = function (id) {
-        return this.$http.get('data/' + id + '.json');
-    };
+//Gets details for a single portfolio piece
+JSONFileDataService.prototype.getPortfolioDetails = function (id) {
+    return this.$http.get('data/' + id + '.json');
+};
 
-//    return JSONFileDataService;
-//})();
 //Registers data service
 app.registerService("dataService", JSONFileDataService);
 
